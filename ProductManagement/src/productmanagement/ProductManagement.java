65e4e2fca042eb689a84e2987d4c25fa786e69d0
kavitamakariye <kmakariye24@gmail.com>
2019-03-09 10:29:25 +0530

    Main Method

---
+++ ProductManagement/src/productmanagement/ProductManagement.java
@@ -0,0 +1,12 @@
+package productmanagement;
+
+import productconsole.ProductConsole;
+
+public class ProductManagement {
+ 
+	public static void main(String[] args) {
+		
+		ProductConsole pc = new ProductConsole();
+		pc.start();
+	}
+}
